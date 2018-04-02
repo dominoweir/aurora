4b00d80fe1d6e1dd972ce102744d86039c3fe76f
Domino Rose Weir <dominoweir@gmail.com>
2018-04-02 13:08:17 -0500

    log in log out functionality added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,16 @@
-function Aurora(){
-  this.initFirebase();
+function initAurora(){
+  // ====================== Menu ======================
+  var signInButton = document.getElementById('sign-in');
+
+  // ====================== Sign-out/Sign-in Handlers ======================
+  signInButton.addEventListener('click', signIn);
+
+  // ====================== Firebase setup ======================
+  initFirebase();
 }
 
 // Sets up shortcuts to Firebase features and initiate firebase auth.
-Aurora.prototype.initFirebase = function() {
-  // Shortcuts to Firebase SDK features.
+function initFirebase() {
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBv6XJb-MnqW_ma4ibMhwuZTdp-o9vR6_k",
@@ -15,79 +21,61 @@ Aurora.prototype.initFirebase = function() {
     messagingSenderId: "650342810200"
   };
   firebase.initializeApp(config);
-  this.auth = firebase.auth();
-  this.database = firebase.database();
-  this.storage = firebase.storage();
-  // Initiates Firebase auth and listen to auth state changes.
-  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+
+  firebase.auth().onAuthStateChanged(onStateChanged);
 };
 
-Aurora.prototype.signIn = function() {
-  // Sign in Firebase using popup auth and Google as the identity provider.
-  var provider = new firebase.auth.GoogleAuthProvider();
-  this.auth.signInWithRedirect(provider);
-};
-
-Aurora.prototype.signOut = function() {
-  // Sign out of Firebase.
-  this.auth.signOut();
-  window.location.reload();
-};
-
-// Triggers when the auth state change for instance when the user signs-in or signs-out.
-Aurora.prototype.onAuthStateChanged = function(user) {
+function onStateChanged() {
+  var user = firebase.auth().currentUser;
   if (user) {
-    //var username = user.displayName;
-    //var profilePicUrl = user.photoURL;
+    // User is signed in.
+    user.providerData.forEach(function (profile) {
+      console.log("  Name: " + profile.displayName);
+      console.log("  Email: " + profile.email);
+    });
+    // hide sign in, show sign out instead
+    document.getElementById('sign-in-text').innerHTML = "Log Out";
 
-    //this.userPic.style.backgroundImage = 'url(' + (profilePicUrl || '/images/profile_placeholder.png') + ')';
-    //this.userName.textContent = userName;
-
-    // Show user's profile and sign-out button.
-    //this.userName.removeAttribute('hidden');
-    //this.userPic.removeAttribute('hidden');
-    //this.signOutButton.removeAttribute('hidden');
-
-    // ====================== Load User Data ======================
-    //this.loadUserSongs();
-
-    // ====================== Edit/Remove Handlers ======================
-
-
-
-    // Hide sign-in button.
-    // this.signInButton.setAttribute('hidden', 'true');
   } else {
+    // User is signed out.
+    document.getElementById('sign-in-text').innerHTML = "Log In";
+  }
+}
 
-    // Hide user's profile and sign-out button.
-    //this.userName.setAttribute('hidden', 'true');
-    //this.userPic.setAttribute('hidden', 'true');
-    //this.signOutButton.setAttribute('hidden', 'true');
-    // Show sign-in button.
-    //this.signInButton.removeAttribute('hidden');
+function signIn() {
+  var user = firebase.auth().currentUser;
+  // if someone is logged in, log them out
+  if (user) {
+    console.log("Sign out user");
+    // Sign out of Firebase.
+    firebase.auth().signOut();
+    window.location.reload();
+  }
+  // else bring up the login popup
+  else{
+    console.log("Sign in user");
+    // Sign in Firebase using popup auth and Google as the identity provider.
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
   }
 };
 
-Aurora.prototype.loadUserSongs = function(){
+function loadUserSongs() {
 
 };
 
-Aurora.prototype.loadAllSongs = function(){
+function loadAllSongs() {
 
 };
 
-Aurora.prototype.addNewSong = function(){
+function addNewSong() {
 
 };
 
-Aurora.prototype.editSongDetails = function(){
+function editSongDetails() {
 
 };
 
-Aurora.prototype.deleteSong = function(){
-
-};
-
-Aurora.prototype.searchSong = function(){
+function deleteSong() {
 
 };
