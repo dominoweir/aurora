a1ca1e7eeb4105e0fad940072b1f4759704f4b5e
Domino Rose Weir <dominoweir@gmail.com>
2018-04-09 14:17:22 -0500

    basically finished with browse

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,82 +1,75 @@
-function initAurora(){
-  // ====================== Menu ======================
-  var signInButton = document.getElementById('sign-in');
-
-  // ====================== Sign-out/Sign-in Handlers ======================
-  signInButton.addEventListener('click', signIn);
-
-  // ====================== Firebase setup ======================
-  initFirebase();
-}
-
-// Sets up shortcuts to Firebase features and initiate firebase auth.
-function initFirebase() {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBv6XJb-MnqW_ma4ibMhwuZTdp-o9vR6_k",
-    authDomain: "aurora-94c4f.firebaseapp.com",
-    databaseURL: "https://aurora-94c4f.firebaseio.com",
-    projectId: "aurora-94c4f",
-    storageBucket: "aurora-94c4f.appspot.com",
-    messagingSenderId: "650342810200"
-  };
-  firebase.initializeApp(config);
-
-  firebase.auth().onAuthStateChanged(onStateChanged);
+var config = {
+  apiKey: "AIzaSyBv6XJb-MnqW_ma4ibMhwuZTdp-o9vR6_k",
+  authDomain: "aurora-94c4f.firebaseapp.com",
+  databaseURL: "https://aurora-94c4f.firebaseio.com",
+  projectId: "aurora-94c4f",
+  storageBucket: "aurora-94c4f.appspot.com",
+  messagingSenderId: "650342810200"
 };
 
-function onStateChanged() {
-  var user = firebase.auth().currentUser;
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+// add sign in listener to sign in button
+var signInButton = document.getElementById('sign-in');
+signInButton.addEventListener('click', signIn);
+
+firebase.auth().onAuthStateChanged(user => {
   if (user) {
     // User is signed in.
-    user.providerData.forEach(function (profile) {
-      console.log("  Name: " + profile.displayName);
-      console.log("  Email: " + profile.email);
-    });
-    // hide sign in, show sign out instead
+    // hide sign in text, show sign out instead
     document.getElementById('sign-in-text').innerHTML = "Log Out";
 
   } else {
     // User is signed out.
     document.getElementById('sign-in-text').innerHTML = "Log In";
   }
-}
+});
 
 function signIn() {
   var user = firebase.auth().currentUser;
-  console.log("Sign in/out");
   // if someone is logged in, log them out
   if (user) {
-    console.log("Sign out user");
     // Sign out of Firebase.
+    firebase.database().goOffline();
     firebase.auth().signOut();
     window.location.reload();
   }
   // else bring up the login popup
   else{
-    console.log("Sign in user");
     // Sign in Firebase using popup auth and Google as the identity provider.
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithRedirect(provider);
   }
 };
 
-function loadUserSongs() {
-
-};
-
 function loadAllSongs() {
 
+  var parent = document.getElementById("song-parent");
+  var download = "<i class='far fa-arrow-alt-circle-down' onclick = 'startDownload()' ></i>";
+  var check = "<i class='far fa-check-circle hide-element'></i>";
+  var span = "<span style='font-weight: 100'>&emsp;by&emsp;</span>";
+
+  // initialize firebase and get songs list
+  var ref = firebase.database().ref("songs/");
+
+  // this doesn't loook like it, but it's basically a for loop
+  ref.once("value")
+  .then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var childData = childSnapshot.val();
+      var artist = childData.artist;
+      var title = childData.title;
+
+      // create a new HTML div element and basically add it to the end of the parent
+      var div = document.createElement("div");
+      div.className = "song";
+      div.innerHTML = title + span + artist + download + check;
+      parent.appendChild(div);
+    });
+  });
 };
 
-function addNewSong() {
-
-};
-
-function editSongDetails() {
-
-};
-
-function deleteSong() {
-
-};
+function startDownload(){
+  console.log("Starting download...");
+}
